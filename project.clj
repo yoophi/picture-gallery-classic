c09c70bfd65e7af22b12ec738e63f311fa8b26e9
Pyunghyuk Yoo <yoophi@gmail.com>
2016-04-16 23:48:59 +0900

    compojure app

--- project.clj
+++ project.clj
@@ -1,6 +1,18 @@
 (defproject picture-gallery "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.6"]
+                 [hiccup "1.0.5"]
+                 [ring-server "0.3.1"]]
+  :plugins [[lein-ring "0.8.12"]]
+  :ring {:handler picture-gallery.handler/app
+         :init picture-gallery.handler/init
+         :destroy picture-gallery.handler/destroy}
+  :profiles
+  {:uberjar {:aot :all}
+   :production
+   {:ring
+    {:open-browser? false, :stacktraces? false, :auto-reload? false}}
+   :dev
+   {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.3.1"]]}})
