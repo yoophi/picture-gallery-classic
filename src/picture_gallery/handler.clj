13308389c321fc7bd978e60e627fa4f627a13700
Pyunghyuk Yoo <yoophi@gmail.com>
2016-04-17 22:44:21 +0900

    setup db, db/create-users-table

--- src/picture_gallery/handler.clj
+++ src/picture_gallery/handler.clj
@@ -1,11 +1,14 @@
 (ns picture-gallery.handler
-  (:require [compojure.core :refer [defroutes routes]]
-            [ring.middleware.resource :refer [wrap-resource]]
-            [ring.middleware.file-info :refer [wrap-file-info]]
-            [hiccup.middleware :refer [wrap-base-url]]
-            [compojure.handler :as handler]
-            [compojure.route :as route]
-            [picture-gallery.routes.home :refer [home-routes]]))
+  (:require
+   [compojure.route :as route]
+   [compojure.core :refer [defroutes]]
+   [noir.util.middleware :as noir-middleware]
+;;   [ring.middleware.resource :refer [wrap-resource]]
+;;   [ring.middleware.file-info :refer [wrap-file-info]]
+;;   [hiccup.middleware :refer [wrap-base-url]]
+;;   [compojure.handler :as handler]
+   [picture-gallery.routes.home :refer [home-routes]]
+   ))
 
 (defn init []
   (println "picture-gallery is starting"))
@@ -17,7 +20,4 @@
   (route/resources "/")
   (route/not-found "Not Found"))
 
-(def app
-  (-> (routes home-routes app-routes)
-      (handler/site)
-      (wrap-base-url)))
+(def app (noir-middleware/app-handler [home-routes app-routes]))
