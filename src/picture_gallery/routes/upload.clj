6eb9a2ab29079b2917d699a70b23c3424b22178d
Pyunghyuk Yoo <yoophi@gmail.com>
2016-04-20 21:47:10 +0900

    make thumbnail

--- src/picture_gallery/routes/upload.clj
+++ src/picture_gallery/routes/upload.clj
@@ -11,41 +11,65 @@
             [clojure.java.io :as io]
             [ring.util.response :refer [file-response]]
             [picture-gallery.models.db :as db]
-            [picture-gallery.util :refer [galleries gallery-path]])
+            [picture-gallery.util :refer [galleries gallery-path
+                                          thumb-prefix
+                                          thumb-size]])
   (:import [java.io File FileInputStream FileOutputStream]
            [java.awt.image AffineTransformOp BufferedImage]
            java.awt.RenderingHints
            java.awt.geom.AffineTransform
            javax.imageio.ImageIO))
 
+(defn scale [img ratio width height]
+  (let [scale        (AffineTransform/getScaleInstance
+                      (double ratio) (double ratio))
+        transform-op (AffineTransformOp.
+                      scale AffineTransformOp/TYPE_BILINEAR)]
+    (.filter transform-op img (BufferedImage. width height (.getType img)))))
+
+(defn scale-image [file]
+  (let [img        (ImageIO/read file)
+        img-width  (.getWidth img)
+        img-height (.getHeight img)
+        ratio      (/ thumb-size img-height)]
+    (scale img ratio (int (* img-width ratio)) thumb-size)))
+
+(defn save-thumbnail [{:keys [filename]}]
+  (let [path (str (gallery-path) File/separator)]
+    (ImageIO/write
+     (scale-image (io/input-stream (str path filename)))
+     "jpeg"
+     (File. (str path thumb-prefix filename)))))
+
 (defn upload-page [info]
   (layout/common
-   [:h2 "Upload an image"]
-   [:p info]
-   (form-to {:enctype "multipart/form-data"}
-            [:post "/upload"]
-            (file-upload :file)
-            (submit-button "upload"))))
+    [:h2 "Upload an image"]
+    [:p info]
+    (form-to {:enctype "multipart/form-data"}
+             [:post "/upload"]
+             (file-upload :file)
+             (submit-button "upload"))))
 
 (defn handle-upload [{:keys [filename] :as file}]
   (let [filename (:filename file)]
     (upload-page
-     (if (empty? filename)
-       (str  "please select a file to upload " file)
+      (if (empty? filename)
+        (str "please select a file to upload " file)
 
-       (try
-         (upload-file (gallery-path) file)
-         (image {:height "150px"}
-                (str "/img/" (url-encode filename)))
-         (catch Exception ex
-           (str "error uploading file " (.getMessage ex))))))))
+        (try
+          (upload-file (gallery-path) file :create-path? true)
+          (save-thumbnail file)
+          (image {:height "150px"}
+                 (str "/img/" (url-encode filename)))
+          (catch Exception ex
+            (str "error uploading file " (.getMessage ex))))))))
 
 (defn serve-file [file-name]
   (file-response (str (gallery-path) File/separator file-name)))
 
 (defroutes upload-routes
-  (GET "/upload" [info] (upload-page info))
+           (GET "/upload" [info] (upload-page info))
 
-  (POST "/upload" [file] (handle-upload file))
+           (POST "/upload" [file] (handle-upload file))
 
-  (GET "/img/:file-name" [file-name] (serve-file file-name)))
+           (GET "/img/:file-name" [file-name] (serve-file file-name)))
--- src/picture_gallery/util.clj
+++ src/picture_gallery/util.clj
@@ -3,7 +3,10 @@
             [hiccup.util :refer [url-encode]])
   (:import java.io.File))
 
+(def thumb-size 150)
+
 (def thumb-prefix "thumb_")
+
 (def galleries "galleries")
 
 (defn gallery-path []
